4f43fa2f345ce62c2e3a7068e5158056849cd0d7
alliance tshinadyi <allytshindayi@gamil.com>
2022-12-15 13:45:26 +0100

    la finalisation de avec la logique et le style du calculette

--- js/script.js
+++ js/script.js
@@ -5,4 +5,48 @@ const listeKeyCode = [];
 for (const num of touches) {
     listeKeyCode.push(num.dataset.key)
 }
-console.log(listeKeyCode);
+const ecran = document.querySelector('.ecran');
+// console.log(listeKeyCode);
+
+
+// Les événements à declanché
+document.addEventListener('keydown', (e) => {
+    // Imprimer la valeur 
+    const valeur = e.keyCode.toString();
+    calculer(valeur);
+    // console.log(valeur, typeof valeur);
+});
+
+document.addEventListener('click', (e) => {
+    // console.log(e);
+    const valeur = e.target.dataset.key;
+    calculer(valeur);
+    // console.log( valeur, typeof valeur);
+});
+
+// CREATION DE LA FONCTION CALCULER
+const calculer = (valeur) => {
+    if(listeKeyCode.includes(valeur)){
+        // console.log(listeKeyCode);
+        switch(valeur){
+            case '8' :
+                ecran.textContent = "";
+                break;
+            case '13':
+                const calcul = eval(ecran.textContent);
+                ecran.textContent = calcul;
+                break;
+
+            default:
+                const indexKeycode = listeKeyCode.indexOf(valeur);
+                const touche = touches[indexKeycode];
+                ecran.textContent += touche.innerHTML; 
+        }
+    }
+}
+
+// Message d'erreur 
+
+window.addEventListener('error', (e) => {
+    alert("Une errreur est survenue dans votre calcul :  " + e.message);
+}) 
